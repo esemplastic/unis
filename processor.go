34a5058000da3729cec9207b4492b9738b3f536a
esemplastic <esemplastic2006@hotmail.com>
2017-05-07 02:08:07 +0300

    :anguished: misspell fix. [Prev: Validators, If, NewMatcher, IsMail]

--- processor.go
+++ processor.go
@@ -23,10 +23,10 @@ func (p ProcessorFunc) Process(original string) (result string) {
 	return p(original)
 }
 
-// OriginProcessor returns a new string processor wich
+// OriginProcessor returns a new string processor which
 // always returns the "original" string back. It does nothing.
 //
-// It can be used as a paramter to the library's functions.
+// It can be used as a parameter to the library's functions.
 var OriginProcessor = ProcessorFunc(func(original string) string { // Is a variable, so the user can change that too.
 	return original
 })
@@ -34,7 +34,7 @@ var OriginProcessor = ProcessorFunc(func(original string) string { // Is a varia
 // ClearProcessor returns a new string processor which always
 // returns empty string back.
 //
-// It can be used as a paramter to the library's functions.
+// It can be used as a parameter to the library's functions.
 var ClearProcessor = ProcessorFunc(func(string) string {
 	return ""
 })
